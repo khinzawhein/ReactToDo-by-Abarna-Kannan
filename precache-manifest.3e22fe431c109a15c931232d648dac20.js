52a843b70cffa852159155632006743c0975f421
Khin Zaw Hein <khinzawhein1989@gmail.com>
2020-09-04 17:17:37 +0630

    Updates

---
+++ precache-manifest.3e22fe431c109a15c931232d648dac20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed28bf22b76456e9a0695103d8f3bc9d",
+    "url": "/khinzawhein/ReactToDo-by-Abarna-Kannan/index.html"
+  },
+  {
+    "revision": "92ba1abdef63562ca685",
+    "url": "/khinzawhein/ReactToDo-by-Abarna-Kannan/static/css/main.31f94903.chunk.css"
+  },
+  {
+    "revision": "22d17d1a0e365fa09123",
+    "url": "/khinzawhein/ReactToDo-by-Abarna-Kannan/static/js/2.fe68eac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/khinzawhein/ReactToDo-by-Abarna-Kannan/static/js/2.fe68eac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ba1abdef63562ca685",
+    "url": "/khinzawhein/ReactToDo-by-Abarna-Kannan/static/js/main.880393b8.chunk.js"
+  },
+  {
+    "revision": "9e4f7e666f75bbef52c7",
+    "url": "/khinzawhein/ReactToDo-by-Abarna-Kannan/static/js/runtime-main.2a1e59b3.js"
+  }
+]);
